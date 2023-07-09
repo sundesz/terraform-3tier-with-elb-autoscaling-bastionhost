5209a2e37a6c7491fb1feef1b9ca33fcd4aa9d5a
Sandesh Hyoju <sundesz@gmail.com>
2023-07-09 20:23:13 +0300

    Add alb security group

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ module "loadbalancing" {
   source         = "./modules/loadbalancing"
   public_subnets = module.networking.public_subnets
   vpc_id         = module.vpc.vpc_id
-  alb_web_sg         = module.networking.alb_web_sg
-  alb_app_sg         = module.networking.alb_app_sg
+  alb_web_sg     = module.networking.alb_web_sg
+  alb_app_sg     = module.networking.alb_app_sg
 }
 
 module "compute" {
